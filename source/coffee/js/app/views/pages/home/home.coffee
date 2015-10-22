5bfcc9661d979f88cdd465b51a5da5565abaa700
Ilya Burakov <brkv@ilya-burakov.local>
2015-10-22 09:34:59 +0300

    Сообщество и прочее

--- source/coffee/js/app/views/pages/home/home.coffee
+++ source/coffee/js/app/views/pages/home/home.coffee
@@ -40,8 +40,8 @@ define ( require ) ->
 						paused 		: true
 					tl
 					.to frontCard 	, 1 	, rotationY : 180		, 0
-					.to backCard	, 1 	, rotationY : 0			, 0
-					.to el			, .5	, z 		: 200		, 0	
+					.to backCard	, 1 	, {width: "100vh", top: 0, left: 0, height: "100vh", rotationY : 0}, 0
+					.to el			, .5	, {position: "absolute", top: 0, left: 0, width: "100vh", height: "100vh"}, 0	
 					#.to el 			, .5	, z 		: 0			, .5
 
 					el.animation = tl
--- src/js/app/views/pages/home/home.js
+++ src/js/app/views/pages/home/home.js
@@ -37,9 +37,17 @@ define(function(require) {
           tl.to(frontCard, 1, {
             rotationY: 180
           }, 0).to(backCard, 1, {
+            width: "100vh",
+            top: 0,
+            left: 0,
+            height: "100vh",
             rotationY: 0
           }, 0).to(el, .5, {
-            z: 200
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100vh",
+            height: "100vh"
           }, 0);
           el.animation = tl;
           return el.animation.play();
