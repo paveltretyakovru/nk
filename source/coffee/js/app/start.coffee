8f94d61af2a0a9e5c9c182dc075f971ad774e3b6
Your Name <you@example.com>
2015-11-13 15:05:44 +0300

    Папка с лэйаутом страниц

--- source/coffee/js/app/start.coffee
+++
@@ -1,8 +0,0 @@
-require [ 'app/app' , 'pace' ] , ( app , pace ) ->
-	'use strict'
-
-	pace.on 'start' , ->
-		console.log 'pace start'
-
-	pace.start
-		document : false
--- src/js/app/start.js
+++
@@ -1,9 +0,0 @@
-require(['app/app', 'pace'], function(app, pace) {
-  'use strict';
-  pace.on('start', function() {
-    return console.log('pace start');
-  });
-  return pace.start({
-    document: false
-  });
-});
